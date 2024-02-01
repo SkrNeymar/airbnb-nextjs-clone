6cb9e5a921d0709c50b72fd70ef71a08330da753
Grant_Cheng <cccguo97@gmail.com>
2024-02-01 17:11:30 +1000

    Add github OAuth login

--- src/components/modals/LoginModal.jsx
+++ src/components/modals/LoginModal.jsx
@@ -87,7 +87,7 @@ const LoginModal = () => {
         outline
         label="Continue with Github"
         icon={AiFillGithub}
-        onClick={() => {}}
+        onClick={() => signIn("github")}
       />
       <div className="mt-4 font-light text-center text-neutral-500">
         <div className="flex flex-row items-center justify-center gap-2">
