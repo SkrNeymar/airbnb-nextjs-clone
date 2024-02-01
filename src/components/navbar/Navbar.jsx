d2fef04e2e749642092594a04ae33e2221db05d7
Grant_Cheng <cccguo97@gmail.com>
2024-02-01 15:18:05 +1000

    Update Navbar component styling: select-none

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
     <div className="fixed z-10 w-full bg-white shadow-sm">
       <div className="py-4 border-b-[1px]">
         <Container>
-          <div className="flex flex-row items-center justify-between gap-3 md:gap-0">
+          <div className="flex flex-row items-center justify-between gap-3 select-none md:gap-0 ">
             <Logo />
             <Search />
             <UserMenu />
